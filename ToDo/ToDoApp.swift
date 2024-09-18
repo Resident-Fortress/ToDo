8ee93e901b3d05bf47f8713cfd09aaf147ffd2ce
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-09-18 10:51:09 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Matthew Dudzinski on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
