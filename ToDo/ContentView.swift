4132e1f9781844c37e5301bf3be86fe91fc5434a
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2024-09-24 10:26:36 -0500

    Added task

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -6,14 +6,55 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State var tasks:Array<Task> = []
+    @Environment (\.modelContext) var context
+    @Query var tasks:Array<Task> = []
+    @State var enteredTask = ""
     var body: some View {
         HeaderView(tasks: $tasks)
-        List(tasks, id: \.self){ tasks in
-            Text("\(tasks.nameOfTask)")
+        HStack{
+            TextField("Enter a task", text: $enteredTask)
+            Button {
+                let task = Task(nameOfTask: enteredTask)
+                context.insert(task)
+                enteredTask = ""
+            } label: {
+                Text("+")
+            }
+
+        }
+        List {
+            ForEach(tasks) { task in
+                Text("\(task.nameOfTask)")
+            }
+            .onDelete(perform: {IndexSet in
+                for index in IndexSet {
+                    context.delete(tasks[index])
+                }})
             }
            
     }
 }
+
+//struct HeaderView: View {
+//    @Binding var tasks:Array<Task>
+//    @Binding var heading:View
+//    @State var enteredString = ""
+//    var body: some View {
+//        HStack{
+//            TextField("Enter a task", text: $enteredString)
+//                .textFieldStyle(.roundedBorder)
+//                .frame(width: 250, alignment: .leading)
+//            Button(action: {
+//                let newTask = Task(nameOfTask: enteredString)
+//                tasks.append(newTask)
+//                enteredString = ""
+//            }, label: {
+//                Image(systemName: "")
+//            })
+//        }
+//    }
+//}
+//
